a4767a7395bda1849eb092b1f5d55db8a7af0e5e
Paul Yuknewicz <paulyuk@Pauls-MBP-2.lan>
2023-03-19 13:14:47 -0700

    initial commit python v2 starter
    Signed-off-by: Paul Yuknewicz <paulyuk@Pauls-MBP-2.lan>

---
+++ starters/python/function_app.py
@@ -0,0 +1,28 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp()
+
+# Learn more at aka.ms/pythonprogrammingmodel
+@app.function_name(name="http")
+@app.route(route="http")
+def test_function(req: func.HttpRequest) -> func.HttpResponse:
+     logging.info('Python HTTP trigger function processed a request.')
+
+     name = req.params.get('name')
+     if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+     if name:
+        hello = f"Hello, {name}. This HTTP triggered function executed successfully."
+        logging.info(hello)
+        return func.HttpResponse(hello)
+     else:
+        hello = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+        logging.info(hello)
+        return func.HttpResponse(hello, status_code=200)
